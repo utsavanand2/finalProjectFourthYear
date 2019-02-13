0544d7d76d73c9a8e8ccf14c67822d358e5fd6ed
Utsav Anand <utsavanand2@gmail.com>
2019-02-13 22:49:20 +0530

    configure for app engine

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use('/static/images/', express.static('public/images'))
 app.get('/:file', (req, res) => {
     res.sendFile(path.join(__dirname + `/html/${req.params.file}`))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
