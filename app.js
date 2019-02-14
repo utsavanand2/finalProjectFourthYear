ab4a57e4339d1406d6c6db4564b60998122c0b0b
Utsav Anand <utsavanand2@gmail.com>
2019-02-14 14:27:57 +0530

    fix root path for web server

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/:file', (req, res) => {
     res.sendFile(path.join(__dirname + `/html/${req.params.file}`))
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html/index.html'))
+})
+
 const port = process.env.PORT || 80
 
 app.listen(port, () => {
